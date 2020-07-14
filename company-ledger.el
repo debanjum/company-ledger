f55c421690ff8e1b535e2b923921bae61ac083c9
debanjum <debanjum@gmail.com>
2020-07-13 22:43:34 -0700

    Remove cl-lib from package-requires list as redundant

--- company-ledger.el
+++ company-ledger.el
@@ -6,7 +6,7 @@
 ;; Description: Fuzzy auto-completion for ledger & friends
 ;; Keywords: abbrev, matching, auto-complete, beancount, ledger, company
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "24.3") (cl-lib "0.5") (company "0.8.0"))
+;; Package-Requires: ((emacs "24.3") (company "0.8.0"))
 ;; URL: https://github.com/debanjum/company-ledger
 
 ;; This file is NOT part of GNU Emacs.
